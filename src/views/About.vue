3d11e6d8782e2ab4219cfa147a61399b8a637a10
dystroyer49ers <34371700+dystroyer49ers@users.noreply.github.com>
2019-03-09 17:05:03 +0000

    Completed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>First vue apllication by me learing from you tube!</p>
+    <p>While been handie caped to learn to be frigaile when typing on apple keyboard so my windows laptop willl survie longer!</p>
   </div>
 </template>
